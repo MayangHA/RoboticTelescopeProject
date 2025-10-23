69e45546919891a4fa38efc63be0348db7f0961b
MayangHA <mayanghermanda@gmail.com>
2025-10-23 16:33:33 +0700

    add feature aws download

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -36,3 +36,34 @@ module.exports = {
   test: config,
   production: config,
 };
+
+// require('dotenv').config();
+// const mysql2 = require('mysql2');
+
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'mysql',
+//     dialectModule: mysql2,
+//   },
+//   test: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'mysql',
+//   },
+//   production: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'mysql',
+//   },
+// };
